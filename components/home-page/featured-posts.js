6d772799673b0aadcf159609df50850cef8f18a8
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-05 20:38:10 +0200

    Starting Work On The "Featured Posts" Part

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,7 +1,12 @@
 import React from "react";
+import classes from "./featured-posts.module.css";
 
 const FeaturedPosts = () => {
-  return <div></div>;
+  return (
+    <section className={classes.latest}>
+      <h2>Featured posts</h2>
+    </section>
+  );
 };
 
 export default FeaturedPosts;
