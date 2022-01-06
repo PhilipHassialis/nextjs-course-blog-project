7581c929f212307126dcc14959d6a25f72747e9f
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-06 12:39:47 +0200

    Rendering Dummy Post Data

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,42 @@ import React from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started",
+    title: "Getting started",
+    image: "gettingstarted.png",
+    excerpt: "NextJS is a React SSR framework",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started2",
+    title: "Getting started",
+    image: "gettingstarted.png",
+    excerpt: "NextJS is a React SSR framework",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started3",
+    title: "Getting started",
+    image: "gettingstarted.png",
+    excerpt: "NextJS is a React SSR framework",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started4",
+    title: "Getting started",
+    image: "gettingstarted.png",
+    excerpt: "NextJS is a React SSR framework",
+    date: "2022-02-10",
+  },
+];
+
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 };
