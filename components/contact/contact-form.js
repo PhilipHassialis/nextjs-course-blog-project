019ec11699d1af6458a8a88610dcf8560c1e194e
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-07 21:50:23 +0200

    Preparing The Contact Form

---
+++ components/contact/contact-form.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classes from "./contact-form.module.css";
+
+const ContactForm = () => {
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form action="" className={classes.form}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your email</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your name</label>
+            <input type="text" id="name" required />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="message">Your message</label>
+          <textarea name="" id="message" cols="30" rows="5"></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Send message</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default ContactForm;
