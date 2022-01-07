a2d98ef06df0a4e19730a5c259249baaf3c395b7
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-07 20:24:06 +0200

    Rendering Code Snippets From Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,6 +3,8 @@ import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const PostContent = (props) => {
   const { post } = props;
@@ -37,6 +39,16 @@ const PostContent = (props) => {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
     },
   };
 
